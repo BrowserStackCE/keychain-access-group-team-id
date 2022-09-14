d66d78fcc06139cca98de92773abe4798d600024
Samiran Saha <samiran.s89@gmail.com>
2022-09-15 01:02:19 +0530

    Initial Commit

---
+++ keychain-access-group-team-id/keychain_access_group_team_idApp.swift
@@ -0,0 +1,17 @@
+//
+//  keychain_access_group_team_idApp.swift
+//  keychain-access-group-team-id
+//
+//  Created by Samiran Saha on 15/09/22.
+//
+
+import SwiftUI
+
+@main
+struct keychain_access_group_team_idApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
